1389bba3b73197511b1df6c7ed534923972376bc
Shantanu <sb5455@nyu.edu>
2018-05-30 14:12:26 +0530

    cleaned unnecessary comments

--- flaskr/flaskr.py
+++ flaskr/flaskr.py
@@ -16,12 +16,6 @@ app.config.from_object(__name__)
 # app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
 
-# def connect_db():
-#     """Connects to the specific database."""
-#     rv = sqlite3.connect(app.config['DATABASE'])
-#     rv.row_factory = sqlite3.Row
-#     return rv
-
 
 @app.route('/')
 def show_form():
