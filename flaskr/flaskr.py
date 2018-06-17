db5bdfe62056849dbce40db8edcc432238fc387d
Hanrui-Chen <37696281+Hanrui-Chen@users.noreply.github.com>
2018-06-17 16:38:07 -0500

    structure
    Build the if else structure

--- flaskr/flaskr.py
+++ flaskr/flaskr.py
@@ -57,6 +57,7 @@ def result():
         if len(list_of_results) == 0:
             return render_template("not_found.html")
         # return str(mapcalc.formatted_google_maps_lines(final_coords))
-        return render_template("heatmap.html", map_center_lat = target_coords[0], map_center_lng = target_coords[1], points_list = mapcalc.formatted_google_maps_lines(final_coords))
+        else:
+            return render_template("heatmap.html", map_center_lat = target_coords[0], map_center_lng = target_coords[1], points_list = mapcalc.formatted_google_maps_lines(final_coords))
 
 
